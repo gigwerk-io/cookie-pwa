0a43ff101a58e325229fda840a7197b5f543fb9d
haron68 <arama006@umn.edu>
2020-09-22 13:15:29 -0700

    Update sign in page

--- src/app/pages/sign-in-with-gigwerk/sign-in-with-gigwerk.module.ts
+++ src/app/pages/sign-in-with-gigwerk/sign-in-with-gigwerk.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -14,6 +14,7 @@ import { SignInWithGigwerkPage } from './sign-in-with-gigwerk.page';
     FormsModule,
     IonicModule,
     SignInWithGigwerkPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [SignInWithGigwerkPage]
 })
--- src/app/pages/sign-in-with-gigwerk/sign-in-with-gigwerk.page.ts
+++ src/app/pages/sign-in-with-gigwerk/sign-in-with-gigwerk.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../utils/services/http/Auth/auth.service';
+import {NavController} from '@ionic/angular';
+import {AlertService} from '../../utils/services/internal/components/alert/alert.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'gig-sign-in-with-gigwerk',
@@ -8,10 +12,43 @@ import { Component, OnInit } from '@angular/core';
 export class SignInWithGigwerkPage implements OnInit {
 
   signInWithGigwerk: boolean = false;
+  signInForm: FormGroup = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  });
 
-  constructor() { }
+  constructor(
+    public authService: AuthService,
+    public navCtrl: NavController,
+    public alertService: AlertService,
+    private fb: FormBuilder
+  ) {
+  }
 
   ngOnInit() {
   }
 
+  signIn() {
+    if (this.signInForm.valid) {
+      this.authService.businessAppLogin(this.signInForm.value)
+      .then(() => {
+        this.navCtrl.navigateRoot('app/home');
+      })
+      .catch(e => {
+        console.log(e);
+        this.alertService.show({
+          duration: 4000,
+          alertMessage: e.error.message,
+          color: 'red',
+          position: 'bottom-0',
+          enterAnimation: 'bottom-slideup',
+          leaveAnimation: 'bottom-slidedown',
+          actionButton: {
+            title: 'Forgot Password',
+            callback: () => console.log('Forgot password!')
+          }
+        });
+      });
+    }
+  }
 }
