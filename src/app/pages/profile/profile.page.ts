9839826e0ca3e250591482a1fadc48ebf3f5acc3
Zion Comer <zioncomer@gmail.com>
2020-09-25 11:23:22 -0700

    Completed search and Profile pages

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { ActivatedRoute } from "@angular/router";
+import { ProfileService } from 'src/app/utils/services/profile/profile.service';
+import {Location} from '@angular/common';
+
 
 @Component({
   selector: 'gig-profile',
@@ -7,15 +11,34 @@ import {Router} from '@angular/router';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-  results: any[];
+
+  email: string;
+  picture: string;
+  last_name: string;
+  first_name: string;
+  username: string;
 
   constructor(
     public router: Router,
+    private route: ActivatedRoute,
+    private profileService: ProfileService,
+    private location: Location
   ) { }
 
-
   ngOnInit() {
-    
+
+    return this.profileService.getProfile(this.route.snapshot.paramMap.get("user_id")).then(res => {
+      console.log(res.data);
+      this.email = res.data['email'];
+      this.picture = res.data['profile']['image'];
+      this.first_name = res.data['first_name'];
+      this.last_name = res.data['last_name'];
+      this.username = res.data['username'];
+    });
+  }
+
+  back() {
+    this.location.back();
   }
 
 }
