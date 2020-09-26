e3eb1b47401ddaccfbb7de3a130c04d1b2bc52fb
haron68 <arama006@umn.edu>
2020-09-26 00:15:16 -0700

    Revert "Completed search and Profile pages"
    This reverts commit 9839826e0ca3e250591482a1fadc48ebf3f5acc3.

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-import { ActivatedRoute } from "@angular/router";
-import { ProfileService } from 'src/app/utils/services/profile/profile.service';
-import {Location} from '@angular/common';
-
 
 @Component({
   selector: 'gig-profile',
@@ -11,34 +7,15 @@ import {Location} from '@angular/common';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-
-  email: string;
-  picture: string;
-  last_name: string;
-  first_name: string;
-  username: string;
+  results: any[];
 
   constructor(
     public router: Router,
-    private route: ActivatedRoute,
-    private profileService: ProfileService,
-    private location: Location
   ) { }
 
+
   ngOnInit() {
-
-    return this.profileService.getProfile(this.route.snapshot.paramMap.get("user_id")).then(res => {
-      console.log(res.data);
-      this.email = res.data['email'];
-      this.picture = res.data['profile']['image'];
-      this.first_name = res.data['first_name'];
-      this.last_name = res.data['last_name'];
-      this.username = res.data['username'];
-    });
-  }
-
-  back() {
-    this.location.back();
+    
   }
 
 }
